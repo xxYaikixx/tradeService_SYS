b77e3bf2709aaca2f8503c2e37a13392a2847fb0
xxYaikixx <peruperuperurin0822@gmail.com>
2023-03-15 14:06:24 +0900

    ユーザー名を入れるようにした

--- src/app/Http/Controllers/Api/ItemController.php
+++ src/app/Http/Controllers/Api/ItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Item;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 use Validator;
 use Illuminate\Support\Facades\Storage;
@@ -36,5 +37,34 @@ class ItemController extends Controller
         $item->change_item_status = $request->itemTargetStatus;
         $item->shipping_method = $request->shippingMethod;
         $item->save();
+    }
+
+    // itemの検索
+    public function search(Request $request)
+    {
+        $itemsQueryBuilder = null;
+        $this->addWhere($itemsQueryBuilder, $request->itemName, 'name');
+        $items = is_null($itemsQueryBuilder) ? Item::all() : $itemsQueryBuilder->get();
+        for ($n = 0; $n<count($items); $n++) {
+            $items[$n]['nickname']=DB::table('users')->find($items[$n]->user_id)->nickname;
+        }
+        return response()->json($items, 200);
+    }
+
+    // クエリビルダの作成
+    private function addWhere(&$itemsQueryBuilder, $param_content, $column)
+    {
+        if (!is_null($param_content)) {
+            if (is_null($itemsQueryBuilder)) {
+                $itemsQueryBuilder =
+                is_array($param_content)
+                ? Item::whereIn($column, $param_content)
+                : Item::where($column, $param_content);
+            } else {
+                is_array($param_content)
+                ? $itemsQueryBuilder->whereIn($column, $param_content)
+                : $itemsQueryBuilder->where($column, $param_content);
+            }
+        }
     }
 }
